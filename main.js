2694b97ba015cc4c36e2d70c3ea8ceba9ea4516b
jennaly <jenna@wicot.io>
2022-06-28 10:15:59 -0700

    6-28-22 innit

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//06/28/22
 // Given an integer array nums, return true if any value appears at least twice in the array, 
 //and return false if every element is distinct.
 /*
@@ -29,3 +30,29 @@
 //return false if true wasn't returned
 
 
+//06/29/22
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+// typically using all the original letters exactly once.
+
+/*
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+ const isAnagram = (s,t) => 
+    s.split('').sort().join('')  === t.split('').sort().join('') ? true: false
+ 
+ //param: 2 strings, ordered differently; 
+ //returns: boolean value, true if they contain all the same letters, false if they don't
+ //question: empty string? 
+ //example: 
+ //Input: s = "anagram", t = "nagaram" => output: true
+ //Input: s = "rat", t = "car" => output: false;
+ //pseudo: split strings into letters in array to use array.prototype.sort() 
+ //arr.sort() arranges letters in array by alphabetical order
+ //join them afterwards into strings again
+ //compare if the two strings are exactly alike
+ //return true if they are the same, false if not
