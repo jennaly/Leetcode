bcbcea9d03bc99af4a83f4b74dd3b55e85f454da
jennaly <106183040+jennaly@users.noreply.github.com>
2022-08-01 15:29:25 -0700

    reorganize challenges into separate files

---
+++ isAnagram.js
@@ -0,0 +1,28 @@
+
+// 242. Valid Anagram
+// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, 
+// typically using all the original letters exactly once.
+
+/*
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+
+// ATTEMPT NO. 1 //
+
+const isAnagram = (s,t) => 
+s.split('').sort().join('')  === t.split('').sort().join('') ? true: false
+
+//param: 2 strings, ordered differently; 
+//returns: boolean value, true if they contain all the same letters, false if they don't
+//question: empty string? 
+//example: 
+//Input: s = "anagram", t = "nagaram" => output: true
+//Input: s = "rat", t = "car" => output: false;
+//pseudo: split strings into letters in array to use array.prototype.sort() 
+//arr.sort() arranges letters in array by alphabetical order
+//join them afterwards into strings again
+//compare if the two strings are exactly alike
+//return true if they are the same, false if not
---
+++ twoSum.js
@@ -0,0 +1,37 @@
+//2 SUM
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// ATTEMPT NO. 1 //
+
+ /*
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+let twoSum = function(nums, target) {
+    let hash = {}; 
+    
+    for (let i = 0; i < nums.length; i++) {
+        let num = nums[i];
+        if (hash[target - num] !== undefined) {
+            return [hash[target-num], i]
+        } else {
+            hash[num] = i;   
+        }
+    }
+    return [];  
+}
+
+//params: array of numbers, target sum
+//questions: empty array? 
+//returns: array of 2 indices of two numbers that add up to target sum, cannot be the same index
+// pseudo:
+// create empty object to keep track of numbers that have been looped through
+// loop through array once, checking to see if the addend is already in the hash table
+// if yes, return the value for the addend key in the hash table and the current index
+// if no, add the current number to the hash table as a key and store its current index as the value
+// return empty array as default if the loop doesn't return anything
